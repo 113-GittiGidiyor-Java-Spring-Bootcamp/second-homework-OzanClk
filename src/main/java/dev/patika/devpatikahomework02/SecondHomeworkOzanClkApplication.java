87d8e7f72c52609817b9a02acff5b614e0d39219
OzanClk <ozancelikinfo@gmail.com>
2021-08-17 09:50:04 +0300

    project initiation

---
+++ src/main/java/dev/patika/devpatikahomework02/SecondHomeworkOzanClkApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.devpatikahomework02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHomeworkOzanClkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHomeworkOzanClkApplication.class, args);
+    }
+
+}
